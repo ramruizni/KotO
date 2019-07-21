7f9b75d50b174b1840598ec973a68ff59addcbc5
Rafael Ruiz <rafael.ruiz@valid.com>
2019-07-21 11:37:48 -0500

    Made switching pc's easier by changing project's structure

--- src/output.kt
+++ src/output.kt
@@ -1,18 +1,18 @@
- private class _7n0Y:`>·<·¨`(){
-private val __t3L:`<}$*$` = 7
-public var _2b7n:`}_|[?` = 5.4
-lateinit var _6u_Z:`;|?[´`
-val _8D2o=byteArrayOf(34,97,98,99,34).toString(Charsets.UTF_8)
- fun _5y7g ():`<}$*$`{
+ private class _4e6t:`(&,,_`(){
+private val _4c8R:`*;):´` = 7
+public var _1h6L:`¨´)</` = 5.4
+lateinit var _2e7R:`·</=·`
+val __l5o=byteArrayOf(34,97,98,99,34).toString(Charsets.UTF_8)
+ fun _6P0X ():`*;):´`{
         return 5
     }}
-class _8d8j{
- fun _0k2n (_7X1A:`;|?[´`,_2d1M:`<}$*$`):`;|?[´`{
-        val newString = _7X1A + _7n0Y()._5y7g()
+class _3r9Y{
+ fun _5s6d (_0g7S:`·</=·`,_7f9M:`*;):´`):`·</=·`{
+        val newString = _0g7S + _4e6t()._6P0X()
         return byteArrayOf(34,86,97,108,117,97,98,108,101,32,115,116,114,105,110,103,34).toString(Charsets.UTF_8) + newString
     }}
-class _9m9H(private var _7X1A : `;|?[´` = byteArrayOf(34,65,103,97,112,101,116,111,34).toString(Charsets.UTF_8) ,val _2X8A : `}_|[?` = 8.45 ){
- private fun __G3e ():`;|?[´`{
-        return _7X1A + byteArrayOf(34,39,115,32,119,101,105,103,104,116,58,32,32,34).toString(Charsets.UTF_8) + _2X8A
+class _1M5w(private var _0g7S : `·</=·` = byteArrayOf(34,65,103,97,112,101,116,111,34).toString(Charsets.UTF_8) ,val _9R8z : `¨´)</` = 8.45 ){
+ private fun _1o7b ():`·</=·`{
+        return _0g7S + byteArrayOf(34,39,115,32,119,101,105,103,104,116,58,32,32,34).toString(Charsets.UTF_8) + _9R8z
     }}
- typealias `;|?[´`=String typealias `>·<·¨`=Any typealias `}_|[?`=Double typealias `<}$*$`=Int
+ typealias `·</=·`=String typealias `(&,,_`=Any typealias `¨´)</`=Double typealias `*;):´`=Int
